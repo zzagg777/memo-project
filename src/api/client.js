d7b544c1f84c1572c47e465f99940f06104b6644
min <zzagg777@gmail.com>
2026-02-13 13:36:45 +0900

    주석코드 삭제

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,6 @@ const PORT = 3001;
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || `${IP}:${PORT}/api`,
-  // baseURL: `${IP}:${PORT}/api`,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
